8d8b1e18864d574e3e2c62bd8bf9225ca67bf44a
Jerry Hou-Liu <jerryhl2001@gmail.com>
2021-10-29 20:30:12 -0700

    Added board size input functionality

--- src/main.java
+++ src/main.java
@@ -1,5 +1,10 @@
 
 public class main {
+	
+	/* Global constants */
+	public static final int MIN_SIZE = 7; //minimum size of the board
+	public static final int MAX_SIZE = 20; //maximum size of the board
+	
 	public static void main(String[] args) {
 		System.out.println("Command line minesweeper project");
 		System.out.println("Please input the size of the board");
